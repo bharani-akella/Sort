6fd455f704da168804b20863b06e197851bc5ab6
Bharani Akella <bharani0akella@gmail.com>
2017-07-06 12:45:36 +0530

    Add files via upload

---
+++ BubbleSortRecursive.java
@@ -0,0 +1,49 @@
+package interview;
+import java.util.Scanner;
+public class BubbleSortRecursive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int size,a[];
+           Scanner s=new Scanner(System.in);
+           System.out.println("Enter the size of array");
+           size=s.nextInt();
+           a=new int[size];
+           
+           for(int i=0;i<size;i++){
+        	   System.out.println("Enter the element at position "+i);
+        	   a[i]=s.nextInt();
+           }
+           
+           System.out.println("Initial array is ");
+          for(int i=0;i<size;i++){
+        	  System.out.print(a[i]+" ");
+          }           
+          
+           BubbleSortRecursive bs=new BubbleSortRecursive();
+           bs.sort(a, size);
+          
+          System.out.println("\nAfter sorting...");
+          for(int i=0;i<size;i++){
+        	  System.out.print(a[i]+" ");
+          }
+          
+	}
+	
+	public void sort(int a[],int size){
+		if(size==1)return;
+		int flag=0;
+		for(int i=0;i<size-1;i++){
+			if(a[i]>a[i+1]){
+				int temp=a[i];
+				a[i]=a[i+1];
+				a[i+1]=temp;
+				flag=1;
+			}
+		}if(flag==0)return;
+		
+		sort(a,size-1);
+		
+	}
+
+}
